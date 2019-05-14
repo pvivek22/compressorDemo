7403b4763dfb7370d593181831fb7ad6ac27f0c7
Prakash <vivek.a.prakash@capgemini.com>
2019-05-14 15:28:45 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ const iotHubClient = require('./IoThub/iot-hub.js');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(function (req, res/*, next*/) {
-//   res.redirect('/');
-// });
+app.use(function (req, res/*, next*/) {
+  res.redirect('/');
+});
 
-app.get('/hello',function(req,res){
-  res.send("Hello World");
-})
+
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
